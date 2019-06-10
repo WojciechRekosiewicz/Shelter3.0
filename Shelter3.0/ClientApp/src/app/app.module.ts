2b92177dbbd7988d55bf024accb3d68140a56b43
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-06-10 18:51:56 +0200

    deleted fetch-data component

--- Shelter3.0/ClientApp/src/app/app.module.ts
+++ Shelter3.0/ClientApp/src/app/app.module.ts
@@ -8,7 +8,6 @@ import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { AdvertsComponent } from './adverts/adverts.component';
 
 
@@ -17,8 +16,7 @@ import { AdvertsComponent } from './adverts/adverts.component';
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
+    CounterComponent, 
     AdvertsComponent
   ],
   imports: [
@@ -28,7 +26,6 @@ import { AdvertsComponent } from './adverts/adverts.component';
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
       { path: 'adverts', component: AdvertsComponent }
     ])
   ],
