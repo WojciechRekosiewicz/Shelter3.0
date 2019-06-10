5587839b97619f941520feea441e96e20e555430
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-06-10 20:10:00 +0200

    added PageNotFoundComponent

--- Shelter3.0/ClientApp/src/app/app.module.ts
+++ Shelter3.0/ClientApp/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { AdvertsComponent } from './adverts/adverts.component';
+import { AdvertComponent } from './adverts/advert/advert.component';
+import { PageNotFoundComponent } from './pageNotFound/pageNotFound.component';
 
 
 @NgModule({
@@ -17,7 +19,9 @@ import { AdvertsComponent } from './adverts/adverts.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent, 
-    AdvertsComponent
+    AdvertsComponent,
+    AdvertComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -26,7 +30,9 @@ import { AdvertsComponent } from './adverts/adverts.component';
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
-      { path: 'adverts', component: AdvertsComponent }
+      { path: 'adverts', component: AdvertsComponent },
+      { path: 'adverts/advert/:id', component: AdvertComponent },
+      { path: '**', component: PageNotFoundComponent }
     ])
   ],
   providers: [],
---
+++ Shelter3.0/ClientApp/src/app/pageNotFound/pageNotFound.component.ts
@@ -0,0 +1,12 @@
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Component({
+  selector: 'pageNotFound',
+  templateUrl: './pageNotFound.component.html',
+  styleUrls: ['./pageNotFound.component.css']
+})
+export class PageNotFoundComponent {
+
+}
