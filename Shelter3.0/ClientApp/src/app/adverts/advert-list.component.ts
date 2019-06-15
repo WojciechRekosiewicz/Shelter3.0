2fc2df83b378f8048ef6dcdaee82071d5a556ccd
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-06-15 17:43:22 +0200

    rework of getting adverts from api in angular

--- Shelter3.0/ClientApp/src/app/adverts/advert-list.component.ts
+++ Shelter3.0/ClientApp/src/app/adverts/advert-list.component.ts
@@ -1,26 +1,44 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Advert } from './advert';
-
+import { AdvertService } from './advert.service';
+import { ActivatedRoute } from '@angular/router';
+import { error } from 'util';
 
 @Component({
   selector: 'app-adverts',
   templateUrl: './advert-list.component.html',
   styleUrls: ['./advert-list.component.css']
 })
-export class AdvertListComponent {
+export class AdvertListComponent implements OnInit {
+
   public adverts: Advert[];
+  errorMessage = '';
     router: any;
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<Advert[]>(baseUrl + 'api/Adverts').subscribe(result => {
-      this.adverts = result;
-    }, error => console.error(error));
+  //constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+  //  http.get<Advert[]>(baseUrl + 'api/Adverts').subscribe(result => {
+  //    this.adverts = result;
+  //  }, error => console.error(error));
+  //}
+
+  constructor(private advertService: AdvertService,
+    private route: ActivatedRoute) {
   }
 
 
   goToAdvertDetails(id) {
     this.router.navigate(['/adverts/advert/', id]);
+  }
+
+
+  ngOnInit(): void {
+    this.advertService.getAdverts().subscribe(
+      adverts => {
+        this.adverts = adverts;
+      },
+      error => this.errorMessage = <any>error
+    );
   }
 }
 
--- Shelter3.0/ClientApp/src/app/adverts/advert.service.ts
+++ Shelter3.0/ClientApp/src/app/adverts/advert.service.ts
@@ -1,52 +1,71 @@
 //import { HttpClient } from 'selenium-webdriver/http';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import { map, catchError, tap } from 'rxjs/operators';
+import { Observable, of, throwError } from 'rxjs';
 import { Advert } from './advert';
 
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class AdvertService {
 
-    constructor(private http: HttpClient ) {
-  }
+    constructor(private http: HttpClient ) {}
 
-//  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-//    http.get<WeatherForecast[]>(baseUrl + 'api/SampleData/WeatherForecasts').subscribe(result => {
-//      this.forecasts = result;
-//    }, error => console.error(error));
-//  }
-//}
-
+  private advertsUrl = 'https://localhost:44378/api/Adverts';
   public adverts: Advert[] = [];
 
-  //public advert: Advert = null;
 
-    loadAdverts(): Observable<boolean> {
-      return this.http.get("https://localhost:44378/api/Adverts")
-            .pipe(
-                map((data: any[]) => {
-                    this.adverts = data;
-                    return true;
-                }));           
-  }
-
-  getAdvert(id: number | string) {
-    return this.loadAdverts().pipe(
-      // (+) before `id` turns the string into a number
-      map((adverts: Advert[]) => adverts.find(advert => advert.advertId === +advert.advertId))
-    );
-  }
-
-  //getAdvert(id: number) Observable<boolean> {
-  //  return this.http.get("https://localhost:44378/api/Adverts/" + id)
-  //    .pipe(
-  //      map((data: any) => {
-  //        this.advert = data;
-  //        return true;
-  //      }));
+  //  loadAdverts(): Observable<boolean> {
+  //    return this.http.get("https://localhost:44378/api/Adverts")
+  //          .pipe(
+  //              map((data: any[]) => {
+  //                  this.adverts = data;
+  //                  return true;
+  //              }));           
   //}
 
+  //getAdvert(id: number | string) {
+  //  return this.loadAdverts().pipe(
+  //    // (+) before `id` turns the string into a number
+  //    map((adverts: Advert[]) => adverts.find(advert => advert.advertId === +advert.advertId))
+  //  );
+  //}
+
+
+  getAdverts(): Observable<Advert[]> {
+    return this.http.get<Advert[]>(this.advertsUrl)
+      .pipe(
+        tap(data => console.log(JSON.stringify(data))),
+        catchError(this.handleError)
+      );
+  }
+
+  getAdvert(id: number): Observable<Advert> {
+    const url = `${this.advertsUrl}/${id}`;
+    return this.http.get<Advert>(url)
+      .pipe(
+      tap(data => console.log('getAdvert: ' + JSON.stringify(data))),
+        catchError(this.handleError)
+      );
+  }
+
+
+  private handleError(err) {
+  // we may send the server to some remote logging infrastructure
+  // instead of just logging it to the console
+  let errorMessage: string;
+  if (err.error instanceof ErrorEvent) {
+    // A client-side or network error occurred. Handle it accordingly.
+    errorMessage = `An error occurred: ${err.error.message}`;
+  } else {
+    // The backend returned an unsuccessful response code.
+    // The response body may contain clues as to what went wrong,
+    errorMessage = `Backend returned code ${err.status}: ${err.body.error}`;
+  }
+  console.error(err);
+  return throwError(errorMessage);
+}
 
 }
